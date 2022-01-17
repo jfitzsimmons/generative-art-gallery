5291d632bc74e9f645350ce89d9584bf2bce24eb
jfitzsimmons <jfitzcode@gmail.com>
2022-01-17 08:32:29 -0600

    style, additional art, favicons

--- lib.js
+++ lib.js
@@ -2,6 +2,11 @@ import {loadArt} from './art/randomcosmos.js'
 import {load} from './art/meditate.js'
 import {loadSyntheosis} from './art/syntheosis.js'
 import {loadTrees} from './art/autumnTrees.js'
+import {loadDabs} from './art/paintDabs.js'
+import {loadCity} from './art/pixelCity.js'
+import {loadMountain} from './art/mountainFog.js'
+import {loadSunset} from './art/oceanSunset.js'
+import {loadWorld} from './art/reclaimedWorld.js'
 
 let buttons = []
 
@@ -9,7 +14,12 @@ const calls = [
     {f:load,name:'meditations'},
     {f:loadArt,name:'cosmos'},
     {f:loadSyntheosis,name: 'syntheosis'},
+    {f:loadDabs,name: 'paint_dabs'},
+    {f:loadCity,name: 'pixel_city'},
     {f:loadTrees,name: 'autumn_trees'},
+    {f:loadMountain,name: 'mountain_fog'},
+    {f:loadSunset,name: 'ocean_sunset'},
+    {f:loadWorld,name: 'reclaimed_world'},
 ];
 let call=0;
 
