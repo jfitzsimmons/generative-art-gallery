25e9e83c89de028636bb9488761cf4822bf0688d
jfitzsimmons <jfitzcode@gmail.com>
2022-04-24 08:16:15 -0500

    html, favicons in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,40 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 module.exports = {
-  mode: "production", 
+  mode: "production",
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Generative Art Gallery',
+      pageHeader: 'Art',
+      random: 'random',
+      shuffle: 'shuffle',
+      pin: 'PIN',
+      another: 'another',
+      again: 'again',
+      sorry: 'Sorry, your browser does not support canvas.',
+      favi: './assets/favicons/favicon.ico',
+      template: './src/index.html',
+    }),
+    new FaviconsWebpackPlugin('./src/assets/favicons/favicon-32x32.png')
+  ],
   devServer: {
     allowedHosts: 'auto',
     client: {
-        overlay: true,
+      overlay: true,
     },
     open: true,
     port: 8000,
     static: {
-        directory: path.join(__dirname, 'dist'),
-        publicPath: '/',
-      },
+      directory: path.join(__dirname, 'dist'),
+      publicPath: '/',
+    },
   },
   module: {
     rules: [
